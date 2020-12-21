16f128d11efb5303004a36e9ee808ac24e310f49
Teddy Odhiambo <todhiambo@microsoft.com>
2020-12-21 03:13:22 +0300

    Added the services interfaces

---
+++ Domain/Services/IGroupService.cs
@@ -0,0 +1,15 @@
+﻿using SCIMServer.Domain.Communication;
+using SCIMServer.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SCIMServer.Domain.Services
+{
+    public interface IGroupService
+    {
+        Task<IEnumerable<Group>> ListAsync();
+        Task<GroupResponse> SaveAsync(Group Group);
+        Task<GroupResponse> UpdateAsync(int id, Group Group);
+        Task<GroupResponse> DeleteAsync(int id);
+    }
+}
---
+++ Domain/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using SCIMServer.Domain.Communication;
+using SCIMServer.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SCIMServer.Domain.Services
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> ListAsync();
+        Task<UserResponse> SaveAsync(User User);
+        Task<UserResponse> UpdateAsync(int id, User User);
+        Task<UserResponse> DeleteAsync(int id);
+    }
+}
